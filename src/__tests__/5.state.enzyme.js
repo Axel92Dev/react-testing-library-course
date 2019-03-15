f2a7f9c8af3d58607a0de23c2a953c7f62b95356
Alessandro Romano <alessandro.romano@lastminute.com>
2019-03-15 14:32:51 +0100

    try to fire event in enzyme

--- src/__tests__/5.state.enzyme.js
+++ src/__tests__/5.state.enzyme.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { render, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { FavoriteNumber } from '../favorite-number';
 
@@ -13,7 +13,7 @@ test('render it with enzyme', () => {
   expect(componentInstance.state.number).toBe(0);
 });
 
-test('test behaviour', () => {
+test('test implementation detail', () => {
   const wrapper = shallow(<FavoriteNumber />);
   const componentInstance = wrapper.instance();
 
@@ -21,4 +21,28 @@ test('test behaviour', () => {
     target: { value: 3 },
   });
   expect(componentInstance.state.number).toBe(3);
+});
+
+test('test behaviour with change', () => {
+  const wrapper = shallow(<FavoriteNumber />);
+  const input = wrapper.find('input');
+
+  input.simulate('change', {
+    target: { value: 3 },
+  });
+
+  expect(wrapper.instance().state.number).toBe(3);
+  // expect(input.props().value).toBe(3);
+});
+
+test('test with render', () => {
+  const wrapper = render(<FavoriteNumber />);
+  const input = wrapper.find('input');
+
+  const changeEvent = document.createEvent('HTMLEvents');
+  changeEvent.initEvent('change', false, true);
+  // document.getElementsByTagName('input')[0].dispatchEvent(changeEvent);
+
+  // expect(input.val()).toBe(3);
+  expect(input.val()).toBe('0');
 });
