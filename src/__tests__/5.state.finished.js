3af1bfd695aa3f500773f854ea89b22ecf4c896f
Alessandro Romano <alessandro.romano@lastminute.com>
2019-03-15 14:39:38 +0100

    improve state testing

--- src/__tests__/5.state.finished.js
+++ src/__tests__/5.state.finished.js
@@ -4,6 +4,13 @@ import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
 import { FavoriteNumber } from '../favorite-number';
 
+test('firing a change updates the value', () => {
+  const { getByLabelText } = render(<FavoriteNumber />);
+  const input = getByLabelText(/favorite number/i);
+  fireEvent.change(input, { target: { value: 3 } });
+  expect(input).toHaveAttribute('value', '3');
+});
+
 test('entering an invalid value shows an error message', () => {
   const { getByLabelText, getByTestId } = render(<FavoriteNumber />);
   const input = getByLabelText(/favorite number/i);
--- src/__tests__/5.state.js
+++ src/__tests__/5.state.js
@@ -6,6 +6,16 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import { FavoriteNumber } from '../favorite-number';
 
+test('firing a change updates the value', () => {
+  const { getByLabelText } = render(<FavoriteNumber />);
+  const input = getByLabelText(/favorite number/i);
+  // 🐨 let's use fireEvent.change to fire a change event on the input to change the value to 3
+  // 📖 learn more here: https://github.com/kentcdodds/react-testing-library/blob/61e382f10d2d8d0be458103b7c267101541ed952/README.md#fireeventnode-htmlelement-event-event
+
+  // 🐨 let's replace this assertion with a new one that verifies the value of the input
+  expect(input).toHaveAttribute('type', 'number');
+});
+
 test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 you're going to need getByTestId (see the favorite-number file and note the data-test attribute).
   const { getByLabelText } = render(<FavoriteNumber />);
